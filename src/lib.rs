2c7e51a99146689539456a67e86ede53de447b48
toshokan <toshokan@shojigate.net>
2020-12-29 12:32:49 -0500

    import bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+mod sys {
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
+    #![allow(dead_code)]
+
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
